d5967bf596b57a6fcc8cf1dd33d205b52474109d
Daniel Batista Galvão <danielbatistagalvao@hotmail.com>
2022-06-06 14:03:54 -0300

    ft/

--- back/src/app.controller.ts
+++ back/src/app.controller.ts
@@ -2,6 +2,7 @@ import { Controller, Get, Request, Post, UseGuards } from '@nestjs/common';
 import { JwtAuthGuard } from './auth/jwt-auth.guard';
 import { LocalAuthGuard } from './auth/local-auth.guard';
 import { AuthService } from './auth/auth.service';
+import { LoginDto } from './login.dto';
 
 @Controller()
 export class AppController {
