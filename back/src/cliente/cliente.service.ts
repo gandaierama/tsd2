561f5ca53d6689b94f8c6a150a42ad8ad934cb67
Daniel Batista Galvão <danielbatistagalvao@hotmail.com>
2022-06-13 09:46:35 -0300

    ft

--- back/src/cliente/cliente.service.ts
+++ back/src/cliente/cliente.service.ts
@@ -17,6 +17,7 @@ export class ClienteService {
 
   async login(body){
 
+      return body;
       const username=body.email;
       const password=body.senha;
       const user = await this.clienteRepository.findOne(username);
