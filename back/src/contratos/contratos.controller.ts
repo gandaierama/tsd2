661ce0c1009956a6a4819670b7509369dcde8a32
Daniel Batista Galvão <danielbatistagalvao@hotmail.com>
2022-06-13 23:02:13 -0300

    ft/

--- back/src/contratos/contratos.controller.ts
+++ back/src/contratos/contratos.controller.ts
@@ -1,4 +1,4 @@
--import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
 import { ContratoService } from './contratos.service';
 import { CreateContratoDto } from './dto/create-contrato.dto';
 import { UpdateContratoDto } from './dto/update-contrato.dto';
