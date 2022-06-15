5c73eefcd0d1c1c841f158571a2870b94725098a
Daniel Batista Galvão <danielbatistagalvao@hotmail.com>
2022-06-15 09:07:46 -0300

    ft

--- admin/pages/api/clientes.js
+++ admin/pages/api/clientes.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 export default async (req, res) => {
-  const url = `http://localhost:3200/cliente/6a0fd432-26b7-4346-aeec-2810df567883`
+  const url = `http://admin.tsdmotoboys.com.br/cliente`
 
   await axios
     .get(url)
