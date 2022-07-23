605e0a908a7d9c2a2afbd16d951d9d2ccfc2a842
Daniel Batista Galvão <danielbatistagalvao@hotmail.com>
2022-07-23 14:27:51 -0300

    ft

--- back/src/motoboys/dto/create-motoboy.dto.ts
+++ back/src/motoboys/dto/create-motoboy.dto.ts
@@ -19,8 +19,7 @@ export class CreateMotoboyDto {
     @ApiProperty({ required: false, default: "991254789645" })
     telefone: string;
 
-    @ApiProperty({ required: false, default: null })
-    endereco: string
+
 
     @ApiProperty({ required: false, default: null })
     banco: string
